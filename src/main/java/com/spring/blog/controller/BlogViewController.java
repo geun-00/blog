53e687f211d8db01bc2edf91f6cc6295df8f32e8
장근영 <jky00914@naver.com>
2024-08-13 21:45:25 +0900

    Blog ArticleList View

---
+++ src/main/java/com/spring/blog/controller/BlogViewController.java
@@ -0,0 +1,29 @@
+package com.spring.blog.controller;
+
+import com.spring.blog.dto.ArticleListViewResponse;
+import com.spring.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
