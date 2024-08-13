dffc070ee332bd3ff6b585dba06bab9a1212adf1
장근영 <jky00914@naver.com>
2024-08-13 21:58:15 +0900

    Blog Article View

--- src/main/java/com/spring/blog/controller/BlogViewController.java
+++ src/main/java/com/spring/blog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.spring.blog.controller;
 
+import com.spring.blog.domain.Article;
 import com.spring.blog.dto.ArticleListViewResponse;
+import com.spring.blog.dto.ArticleViewResponse;
 import com.spring.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,5 +28,14 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
