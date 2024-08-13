e4cec19bc785da8eae0fe61661840fb96e19c0cc
장근영 <jky00914@naver.com>
2024-08-13 16:15:29 +0900

    Get All Articles API

--- src/main/java/com/spring/blog/controller/BlogApiController.java
+++ src/main/java/com/spring/blog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.spring.blog.controller;
 
 import com.spring.blog.domain.Article;
 import com.spring.blog.dto.AddArticleRequest;
+import com.spring.blog.dto.ArticleResponse;
 import com.spring.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +25,15 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
