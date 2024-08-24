2c173601251eab40277e53d1695ecf9509b81022
장근영 <jky00914@naver.com>
2024-08-24 18:24:34 +0900

    User 뷰&API 컨트롤러 분리

--- src/main/java/com/spring/blog/controller/UserApiController.java
+++ src/main/java/com/spring/blog/controller/UserApiController.java
@@ -1,8 +1,6 @@
 package com.spring.blog.controller;
 
 import com.spring.blog.common.annotation.CurrentUser;
-import com.spring.blog.controller.validator.AddUserValidator;
-import com.spring.blog.dto.AddUserRequest;
 import com.spring.blog.model.PrincipalUser;
 import com.spring.blog.service.UserService;
 import jakarta.servlet.http.Cookie;
@@ -11,47 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserApiController {
 
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
-        dataBinder.addValidators(addUserValidator);
-    }
-
     private final UserService userService;
-    private final AddUserValidator addUserValidator;
-
-    @PostMapping("/user")
-    public String signup(@Validated @ModelAttribute AddUserRequest request, BindingResult bindingResult,
-                         @CurrentUser Authentication authentication) {
-
-        if (bindingResult.hasErrors()) {
-            return "signup";
-        }
-
-        //폼 인증 가입
-        if (authentication == null) {
-            userService.save(request);
-        }
-        //OAuth 인증 가입, 별명만 재설정
-        else {
-            PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
-            userService.updateNickname(request.getNickname(), principalUser.getEmail());
-        }
-
-        return "redirect:/login?success";
-    }
 
     @DeleteMapping("/user")
     public ResponseEntity<Void> deleteUser(@CurrentUser Authentication authentication,
--- src/main/java/com/spring/blog/controller/UserViewController.java
+++ src/main/java/com/spring/blog/controller/UserViewController.java
@@ -1,13 +1,34 @@
 package com.spring.blog.controller;
 
+import com.spring.blog.common.annotation.CurrentUser;
+import com.spring.blog.controller.validator.AddUserValidator;
 import com.spring.blog.dto.AddUserRequest;
+import com.spring.blog.model.PrincipalUser;
+import com.spring.blog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class UserViewController {
+
+    private final UserService userService;
+    private final AddUserValidator addUserValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(addUserValidator);
+    }
 
     @GetMapping("/login")
     public String login(@RequestParam(name = "error", required = false) String error,
@@ -23,5 +44,26 @@ public class UserViewController {
     public String signup(Model model) {
         model.addAttribute("addUserRequest", new AddUserRequest());
         return "signup";
+    }
+
+    @PostMapping("/user")
+    public String signup(@Validated @ModelAttribute AddUserRequest request, BindingResult bindingResult,
+                         @CurrentUser Authentication authentication) {
+
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        //폼 인증 가입
+        if (authentication == null) {
+            userService.save(request);
+        }
+        //OAuth 인증 가입, 별명만 재설정
+        else {
+            PrincipalUser principalUser = (PrincipalUser) authentication.getPrincipal();
+            userService.updateNickname(request.getNickname(), principalUser.getEmail());
+        }
+
+        return "redirect:/login?success";
     }
 }
