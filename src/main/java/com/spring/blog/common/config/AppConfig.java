156eabfde7ffac26339209f5c91f346ee62b5cf9
장근영 <jky00914@naver.com>
2024-08-25 17:11:26 +0900

    Querydsl 추가

--- src/main/java/com/spring/blog/common/config/AppConfig.java
+++ src/main/java/com/spring/blog/common/config/AppConfig.java
@@ -1,15 +1,26 @@
 package com.spring.blog.common.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final EntityManager em;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
 }
