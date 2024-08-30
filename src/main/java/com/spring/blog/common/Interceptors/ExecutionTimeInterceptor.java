e5b059cd10fdb8f2f50d316b6a5aa5aae57d7dba
장근영 <jky00914@naver.com>
2024-08-30 18:05:41 +0900

    실행시간 측정 인터셉터 등록

---
+++ src/main/java/com/spring/blog/common/Interceptors/ExecutionTimeInterceptor.java
@@ -0,0 +1,31 @@
+package com.spring.blog.common.Interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class ExecutionTimeInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        log.info("[{}] ({}) executed in {} ms", request.getMethod(), request.getRequestURI(), executionTime);
+        if (ex != null) {
+            log.error("{}", ex.getMessage());
+        }
+    }
+}
--- src/main/java/com/spring/blog/common/config/WebMvcConfig.java
+++ src/main/java/com/spring/blog/common/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.spring.blog.common.config;
 
+import com.spring.blog.common.Interceptors.ExecutionTimeInterceptor;
 import com.spring.blog.common.argumentResolver.CurrentUserArgumentResolver;
 import com.spring.blog.common.argumentResolver.UserKeyArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CurrentUserArgumentResolver());
         resolvers.add(new UserKeyArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ExecutionTimeInterceptor())
+                .excludePathPatterns("/images/**", "/css/**", "/js/**", "/style/**");
     }
 }
