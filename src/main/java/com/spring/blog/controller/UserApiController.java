3d5dec76e9427adc278036d455ad6b0e9a67acf1
장근영 <jky00914@naver.com>
2024-08-31 20:24:50 +0900

    회원 탈퇴 안되는 버그 수정

--- src/main/java/com/spring/blog/controller/UserApiController.java
+++ src/main/java/com/spring/blog/controller/UserApiController.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserApiController {
 
