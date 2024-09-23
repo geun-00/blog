0b964ceae9f6a2f5c8c2100ad9ef79ce1e876539
장근영 <jky00914@naver.com>
2024-09-23 22:44:06 +0900

    설정 파일 환경별로 분리

--- src/main/java/com/spring/blog/controller/api/LocalFileController.java
+++ src/main/java/com/spring/blog/controller/api/LocalFileController.java
@@ -21,7 +21,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-@Profile("test")
+@Profile({"test", "local"})
 @RestController
 @RequiredArgsConstructor
 public class LocalFileController {
