8329512d958718f143344d4441f532ea19a8af78
장근영 <jky00914@naver.com>
2024-09-24 14:33:42 +0900

    S3Config @Profile 수정

--- src/main/java/com/spring/blog/common/config/configs/S3Config.java
+++ src/main/java/com/spring/blog/common/config/configs/S3Config.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("local")
+@Profile({"prod", "local"})
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
