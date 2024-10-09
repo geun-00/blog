3f78153be627841be63224e5e3787ee3d8b314da
장근영 <jky00914@naver.com>
2024-10-09 21:49:12 +0900

    통합 테스트 구조 수정

--- src/test/java/com/spring/blog/controller/ApiControllerIntegrationTestSupport.java
+++ src/test/java/com/spring/blog/controller/ApiControllerIntegrationTestSupport.java
@@ -1,17 +1,13 @@
 package com.spring.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.spring.blog.BlogApplication;
+import com.spring.blog.IntegrationTestSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(classes = BlogApplication.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-public abstract class ApiControllerIntegrationTestSupport {
+public abstract class ApiControllerIntegrationTestSupport extends IntegrationTestSupport {
 
     @Autowired
     protected MockMvc mockMvc;
--- src/test/java/com/spring/blog/controller/ViewControllerIntegrationTestSupport.java
+++ src/test/java/com/spring/blog/controller/ViewControllerIntegrationTestSupport.java
@@ -1,20 +1,16 @@
 package com.spring.blog.controller;
 
-import com.spring.blog.BlogApplication;
+import com.spring.blog.IntegrationTestSupport;
 import com.spring.blog.repository.ArticleLikesRepository;
 import com.spring.blog.repository.BlogRepository;
 import com.spring.blog.repository.BulkInsertRepository;
 import com.spring.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(classes = BlogApplication.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-public abstract class ViewControllerIntegrationTestSupport {
+public abstract class ViewControllerIntegrationTestSupport extends IntegrationTestSupport {
 
     @Autowired
     protected MockMvc mockMvc;
